8e3a0a3558b1c84e6b3607dbab439c6f56d654c2
shivamv97 <shivamkv97@gmail.com>
2024-11-15 02:14:42 +0530

    cra and tailwind

---
+++ .history/src/App_20241114142540.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">Shivam Verma Let's go!!</div>;
+}
+
+export default App;
---
+++ .history/src/App_20241115015318.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="text-3xl">Shivam Verma Let's go!!</div>;
+}
+
+export default App;
